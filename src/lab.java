4559f7a2225727e32a00ca1d3887574c17364fd9
xichen <xichenf2@illinois.edu>
2018-02-06 18:37:08 -0600

    prince

--- src/lab.java
+++ src/lab.java
@@ -1,5 +1,39 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
 public class lab {
-    public static void main (String[] unused){
-        System.out.print("");
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
     }
+
+    public static void main(String[] unused) {
+
+
+        String hamlet = urlToString("http://erdani.com/tdpl/hamlet.txt");
+        String[] wordCount = hamlet.split(" ");
+        int totalWorde = wordCount.length;
+        System.out.print(totalWorde);
+
+        hamlet.replaceAll("[Prince]","@");
+        hamlet.replaceAll("[^@]","");
+        int totalPrince = hamlet.length();
+        System.out.print(totalPrince);
+
+    }
+
 }
